917fe41dc1071789d246b75c6c0c0cf160d2bebe
Vinny3216 <W0496645@campus.nscc.ca>
2026-03-26 20:15:29 -0300

    Add prescription detail screen placeholder

---
+++ medminder/lib/screens/prescription_detail_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PrescriptionDetailScreen extends StatelessWidget {
+  const PrescriptionDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Prescription Details'),
+      ),
+      body: const Center(
+        child: Text('Prescription Detail Page - Coming Soon'),
+      ),
+    );
+  }
+}
